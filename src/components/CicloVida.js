ff236e59df3ee7ff9190ec849835a2917c00d731
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2021-03-19 23:08:09 -0400

    Ciclo de Vida en un Class Component, Montaje y Actualización

--- src/components/CicloVida.js
+++ src/components/CicloVida.js
@@ -1,16 +1,3 @@
-import React, { Component } from 'react';
-
-export default class CicloVida extends Component {
-    render() {
-        return(
-            <>
-                <h2>Ciclo de Vida de React</h2>
-            </>
-        );
-    }
-}
-
-
 /* 
     Ciclo de Vida 
     
@@ -56,3 +43,61 @@ export default class CicloVida extends Component {
         *   componentWillUnmount(). Se ejecuta antes de destruir el componente del árbol del
             DOM, es un método útil para hacer tareas de limpieza.
 */
+
+import React, { Component } from 'react';
+
+export default class CicloVida extends Component {
+    constructor(props) {
+        super(props);
+        console.log(0, "El componente se inicializa, aún NO está en el DOM");
+
+        this.state = {
+            hora : new Date().toLocaleTimeString(),
+        };
+
+        this.temporizador = null;
+    }
+
+    componentDidMount() {
+        console.log(1, "El componente ya se encuentra en el DOM");
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log(2, "El estado o las props han cambiado");
+        console.log("prevProps: ", prevProps); // Muestra las props previas que maneja el componente en este caso un objeto vacio.
+        console.log("prevState: ", prevState); // Muestra el estado previo antes de la actualización del componente.
+    }
+
+    tictac = () => {
+        this.temporizador = setInterval(() => {
+            this.setState({
+                hora : new Date().toLocaleTimeString(),
+            });
+        }, 1000);
+    }
+    
+    iniciar = () => {
+        this.tictac();
+    }
+
+    detener = () => {
+        clearInterval(this.temporizador);
+    }
+    render() {
+        console.log(4, "El componente se dibuja (o redibuja por algun cambio) en el DOM");
+        return(
+            <>
+                <h2>Ciclo de Vida de los Componentes de Clase</h2>
+
+                <h3>{ this.state.hora }</h3>
+
+                <button onClick = { this.iniciar }>Iniciar</button>
+                <button onClick = { this.detener }>Detener</button>
+            </>
+        );
+    }
+}
+
+
+
+
